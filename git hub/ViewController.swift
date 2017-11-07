982dd3682d69b1cd575186ed12fd04582c3e7efc
Gorkem İskenderoglu <gorkemiskenderoglu@Gorkem-MacBook-Air.local>
2017-11-07 21:34:40 +0300

    Initial Commit

---
+++ git hub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git hub
+//
+//  Created by Gorkem İskenderoglu on 7.11.2017.
+//  Copyright © 2017 Gorkem İskenderoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
